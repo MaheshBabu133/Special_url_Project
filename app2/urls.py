23ae0b3bbca299900126094ca7c75b1d515b8e74
MaheshBabu133 <maheshbabuntr123@gmail.com>
2023-03-08 21:23:39 +0530

    first commit

---
+++ app2/urls.py
@@ -0,0 +1,7 @@
+from app2.views import *
+from django.urls import path
+app_name='nothing'
+urlpatterns=[
+    path('Mahesh/',Mahesh,name='Mahesh'),
+    path('Afrin/',Afrin,name='Afrin'),
+]
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+# Create your views here.
+def Mahesh(request):
+    return HttpResponse("<h1 style='color:red;'>My name is Mahesh</h1>")
+def Afrin(request):
+    return HttpResponse("<h1 style='color:blue;'>My name is Afrin</h1>")
